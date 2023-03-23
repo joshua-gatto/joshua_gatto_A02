bc983842d749b8de434fe0052b5a7d3a1d6209da
joshua-gatto <joshuagatto@cmail.carleton.ca>
2023-03-23 13:50:54 -0400

    initial commit

---
+++ processProfile.php
@@ -0,0 +1,17 @@
+<?php
+if(isset($_POST["submit"])){
+    include("connection.php");
+
+    define("DATABASE_LOCAL", "localhost");
+    define("DATABASE_NAME", "joshua_gatto_syscbook");
+    define("DATABASE_USER", "root");
+    define("DATABASE_PASSWD", "");
+
+    $conn = new mysqli(DATABASE_LOCAL, DATABASE_USER, DATABASE_PASSWD, DATABASE_NAME);
+    if ($conn->connect_error) {
+        echo "Error";
+        die("Connection failed: " . $conn->connect_error);
+    }else{}
+    $conn->close();
+}
+?>
